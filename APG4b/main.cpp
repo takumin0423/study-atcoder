f6a6ac433023437d93c53470425a07b62fd632a5
takumin0423 <takumin0423baske@gmail.com>
2024-11-23 23:00:47 +0900

    EX7 - bool値パズル

--- APG4b/main.cpp
+++ APG4b/main.cpp
@@ -2,19 +2,32 @@
 using namespace std;
 
 int main() {
-  int A, B;
-  string op;
-  cin >> A >> op >> B;
+  // 変数a,b,cにtrueまたはfalseを代入してAtCoderと出力されるようにする。
+  bool a = true;   // true または false
+  bool b = false;  // true または false
+  bool c = true;   // true または false
 
-  if (op == "+") {
-    cout << A + B << endl;
-  } else if (op == "-") {
-    cout << A - B << endl;
-  } else if (op == "*") {
-    cout << A * B << endl;
-  } else if (op == "/" && B != 0) {
-    cout << A / B << endl;
+  // ここから先は変更しないこと
+
+  if (a) {
+    cout << "At";
   } else {
-    cout << "error" << endl;
+    cout << "Yo";
   }
+
+  if (!a && b) {
+    cout << "Bo";
+  } else if (!b || c) {
+    cout << "Co";
+  }
+
+  if (a && b && c) {
+    cout << "foo!";
+  } else if (true && false) {
+    cout << "yeah!";
+  } else if (!a || c) {
+    cout << "der";
+  }
+
+  cout << endl;
 }
